c169c9b296dbfee2a247e96eb9bf3e91e8c5b2e0
Denys Markevych <denysjse@gmail.com>
2024-07-14 16:00:42 +0300

    Create logic for check user role (user or guest)

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,6 +4,8 @@ import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { Provider } from 'react-redux'
 import { PersistGate } from 'redux-persist/integration/react'
 import { persistor, store } from '@/store/store'
+import AuthProvider from '@/providers/auth-provider/AuthProvider'
+import { TypeComponentAuthFields } from '@/providers/auth-provider/auth-page.types'
 
 const queryClient = new QueryClient({
 	defaultOptions: {
@@ -13,12 +15,17 @@ const queryClient = new QueryClient({
 	}
 })
 
-export default function App({ Component, pageProps }: AppProps) {
+export default function App({
+	Component,
+	pageProps
+}: AppProps & TypeComponentAuthFields) {
 	return (
 		<QueryClientProvider client={queryClient}>
 			<Provider store={store}>
 				<PersistGate loading={null} persistor={persistor}>
-					<Component {...pageProps} />
+					<AuthProvider Component={{ isOnlyUser: Component.isOnlyUser }}>
+						<Component {...pageProps} />
+					</AuthProvider>
 				</PersistGate>
 			</Provider>
 		</QueryClientProvider>
