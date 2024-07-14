bcb90ec1cd980f64ff495e69973d87faf1b3761c
Denys Markevych <denysjse@gmail.com>
2024-07-14 19:34:02 +0300

    Create auth page and realize logic for login to the system

---
+++ src/components/ui/button/Button.tsx
@@ -0,0 +1,31 @@
+import { ButtonHTMLAttributes, FC, PropsWithChildren } from 'react'
+import cn from 'clsx'
+
+interface IButton extends ButtonHTMLAttributes<HTMLButtonElement> {
+	variant: 'orange' | 'light'
+}
+
+const Button: FC<PropsWithChildren<IButton>> = ({
+	children,
+	className,
+	variant,
+	...rest
+}) => {
+	return (
+		<button
+			{...rest}
+			className={cn(
+				'rounded-xl font-bold shadow px-10 py-2',
+				{
+					'text-white bg-primary': variant === 'orange',
+					'text-primary bg-white': variant === 'light'
+				},
+				className
+			)}
+		>
+			{children}
+		</button>
+	)
+}
+
+export default Button
